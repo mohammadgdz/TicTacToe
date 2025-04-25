66939fc3c237ee256afedc9836f89a3836f727f1
mohammadgdz <mhd.gdz@gmail.com>
2025-04-25 12:01:18 +0330

    Initial Commit

---
+++ tiktoc/tiktocApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktocApp.swift
+//  tiktoc
+//
+//  Created by Mohammad on 2025-04-25.
+//
+
+import SwiftUI
+
+@main
+struct tiktocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
